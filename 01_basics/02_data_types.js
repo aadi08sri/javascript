c5171decb0aaaeeba2a005760d9f9791295b0d1c
aadi08sri <aaditya08srivastava@gmail.com>
2025-06-12 11:54:37 +0000

    datatypes

---
+++ 01_basics/02_data_types.js
@@ -0,0 +1,30 @@
+
+"use strict"; // treat all JS code as newer version(no need to mention explicitly)
+
+// alert( 3 + 3) // we are using nodejs, not browser(thats why it is showing error without console)
+
+console.log(3 + 3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+/* RANGES */
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+//typeof operator is used to know the datatype explicitly
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
